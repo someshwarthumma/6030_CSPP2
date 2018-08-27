844856f7e62663020c8aaf8f262c9fec2f452003
someshwarthuma <somesh.sandeep619@msitprogram.net>
2018-08-27 22:38:10 +0530

    Quotient and remainder

---
+++ CSPP2-practice/m1/quotient_remainder.java
@@ -0,0 +1,8 @@
+public class quotient_remainder{
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 2;
+		System.out.println("Quotient is :"+a/b);
+		System.out.println("Remainder is: "+ a%b);
+	}
+}
