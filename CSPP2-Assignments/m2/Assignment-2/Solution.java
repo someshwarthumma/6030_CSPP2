b959ff3259b207b5c809e0867d11c115b4b7ce60
someshwarthuma <somesh.sandeep619@msitprogram.net>
2018-08-28 15:34:35 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- CSPP2-Assignments/m2/Assignment-2/Solution.java
+++ CSPP2-Assignments/m2/Assignment-2/Solution.java
@@ -4,32 +4,32 @@ import java.util.Scanner;
  * Class for solution.
  */
 public class Solution {
-	/**.
-	 * solution class to find the solution
-	 *
-	 * @param      args  These are arguments
-	 */
-	public static final void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int a = scan.nextInt();
-		int b = scan.nextInt();
-		int c = scan.nextInt();
-		rootsOfQuadraticEquation(a, b, c);
-	}
-	/**
-	 * It gives
-	 *
-	 * @param      a     integer
-	 * @param      b     integer
-	 * @param      c     integer
-	 */
-	public static void rootsOfQuadraticEquation(final int a, final int b, final int c) {
-		double x;
-		double y;
-		final int z = 4;
-		x = (-b + Math.sqrt((b * b) - z * a * c)) / (2 * a);
-		y = (-b - Math.sqrt((b * b) - z * a * c)) / (2  * a);
-		System.out.println(x + " " + y);
-	}
+    /**.
+     * solution class to find the solution
+     *
+     * @param      args  These are arguments
+     */
+    public static final void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        rootsOfQuadraticEquation(a, b, c);
+    }
+    /**
+     * It gives
+     *
+     * @param      a     integer
+     * @param      b     integer
+     * @param      c     integer
+     */
+    public static void rootsOfQuadraticEquation(final int a, final int b, final int c) {
+        double x;
+        double y;
+        final int z = 4;
+        x = (-b + Math.sqrt((b * b) - z * a * c)) / (2 * a);
+        y = (-b - Math.sqrt((b * b) - z * a * c)) / (2  * a);
+        System.out.println(x + " " + y);
+    }
 }
 
