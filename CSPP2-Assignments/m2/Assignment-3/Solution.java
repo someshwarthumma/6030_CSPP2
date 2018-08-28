900b0039c8b786c8a123fa6c41738ad54ea68691
someshwarthuma <somesh.sandeep619@msitprogram.net>
2018-08-28 19:18:31 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- CSPP2-Assignments/m2/Assignment-3/Solution.java
+++ CSPP2-Assignments/m2/Assignment-3/Solution.java
@@ -3,38 +3,38 @@ import java.util.Scanner;
  * This is main class for solution
  */
 public final class Solution {
-	/**.
-	 * This is constructor
-	 */
-	private Solution(){
-		// This is private
-	}
-	/**.
-	 *This is main method
-	 *
-	 * @param      args  this is argument
-	 */
-	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		long base = s.nextInt();
-		long exponent = s.nextInt();
-		long result = power(base, exponent);
-		System.out.println(result);
-	}
-	/**.
-	 * To calculate the power of a given base upto given exponent
-	 *
-	 * @param      base      long
-	 * @param      exponent  long
-	 *
-	 * @return     It return the long type 
-	 */
-	public static long power(long base, long exponent) {
-		if (exponent != 0) {
-			return (base * power(base, exponent - 1));
-		} else {
-			return 1;
-		}
+    /**.
+     * This is constructor
+     */
+    private Solution() {
+        // This is private
+    }
+    /**.
+     *This is main method
+     *
+     * @param      args  this is argument
+     */
+    public static final void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        long base = s.nextInt();
+        long exponent = s.nextInt();
+        long result = power(base, exponent);
+        System.out.println(result);
+    }
+    /**.
+     * To calculate the power of a given base upto given exponent
+     *
+     * @param      base      long
+     * @param      exponent  long
+     *
+     * @return     It return the long type
+     */
+    public static long power(final long base, final long exponent) {
+        if (exponent != 0) {
+            return (base * power(base, exponent - 1));
+        } else {
+            return 1;
+        }
 
-	}
+    }
 }
