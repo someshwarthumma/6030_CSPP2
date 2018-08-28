2ef963d81ac98df66f63c8a500f2ab0e48d15ac9
someshwarthuma <somesh.sandeep619@msitprogram.net>
2018-08-28 15:36:52 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- CSPP2-Assignments/m2/Assignment-2/Solution.java
+++ CSPP2-Assignments/m2/Assignment-2/Solution.java
@@ -14,7 +14,7 @@ public class Solution {
         int a = scan.nextInt();
         int b = scan.nextInt();
         int c = scan.nextInt();
-        rootsOfQuadraticEquation(a, b, c);
+        Equation(a, b, c);
     }
     /**
      * It gives
@@ -23,7 +23,7 @@ public class Solution {
      * @param      b     integer
      * @param      c     integer
      */
-    public static void rootsOfQuadraticEquation(final int a, final int b, final int c) {
+    public static void Equation(final int a, final int b, final int c) {
         double x;
         double y;
         final int z = 4;
