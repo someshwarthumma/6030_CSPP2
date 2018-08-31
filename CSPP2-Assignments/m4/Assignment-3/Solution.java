9437739ca6723136f64a3bf94c9f3c8e9a86750f
someshwarthuma <somesh.sandeep619@msitprogram.net>
2018-08-31 16:25:22 +0530

    "check_style_score -> 1 of 1 passed. style score: 1/1 "

--- CSPP2-Assignments/m4/Assignment-3/Solution.java
+++ CSPP2-Assignments/m4/Assignment-3/Solution.java
@@ -3,44 +3,45 @@ import java.util.Scanner;
  * Class for solution.
  */
 public final class Solution {
-	/**.
-	 * This is construction
-	 */
-	private Solution() {
-		//This is solution constructor
-	}
-	/**.
-	 * { function_description }
-	 *
-	 * @param      args  These are arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		sc.nextLine();
-		for (int i = 0; i < n; i++) {
-			String s = sc.nextLine();
-			int res = binaryToDecimal(s); //Write binaryToDecimal function
-			System.out.println(res);
-		}
-	}
-	/**.
-	 * { function_description }
-	 *
-	 * @param      s     string
-	 *
-	 * @return     integer
-	 */
-	public static int binaryToDecimal(final String s) {
-		int[] num = new int[s.length()];
-		for (int i = 0; i < s.length(); i++) {
-			num[i] = Integer.parseInt(s.charAt(i) + "");
-		}
-		int sum = 0;
-		for (int i = s.length() - 1; i >= 0; i--) {
-			sum = sum + (int) (num[i] * Math.pow(2, ((s.length()) - (i + 1))));
-		}
-		return sum;
-	}
+    /**.
+     * This is construction
+     */
+    private Solution() {
+        //This is solution constructor
+    }
+    /**.
+     * { function_description }
+     *
+     * @param      args  These are arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+        for (int i = 0; i < n; i++) {
+            String s = sc.nextLine();
+            int res = binaryToDecimal(s);
+            System.out.println(res);
+        }
+    }
+    /**.
+     * { function_description }
+     *
+     * @param      s     string
+     *
+     * @return     integer
+     */
+    public static int binaryToDecimal(final String s) {
+        int[] num = new int[s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            num[i] = Integer.parseInt(s.charAt(i) + "");
+        }
+        int sum = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            sum = sum + (int) (num[i] * Math.pow(2,
+                        ((s.length()) - (i + 1))));
+        }
+        return sum;
+    }
 
 }
