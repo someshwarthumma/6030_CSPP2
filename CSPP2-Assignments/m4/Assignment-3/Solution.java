7baf1a592063f5977d03ae3a2e4a6f668621a688
someshwarthuma <somesh.sandeep619@msitprogram.net>
2018-08-31 16:07:01 +0530

    "check_style_score -> 1 of 1 passed. style score: 0/1 "

--- CSPP2-Assignments/m4/Assignment-3/Solution.java
+++ CSPP2-Assignments/m4/Assignment-3/Solution.java
@@ -1,31 +1,45 @@
-
-// Given an String, Write a java method that returns the decimal value for the given binary string.
-
-
 import java.util.Scanner;
-public class Solution
-{/*
+/**.
+ * Class for solution.
+ */
+public final class Solution {
+	/*.
 	Do not modify this main function.
 	*/
-	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		int n= sc.nextInt();
+	private Solution(){
+		//This is solution constructor
+	}
+	/**.
+	 * { function_description }
+	 *
+	 * @param      args  These are arguments
+	 */
+	public static void main(final String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
 		sc.nextLine();
-		for(int i=0;i<n;i++){
-			String s=sc.nextLine();
-			int res=binaryToDecimal(s);//Write binaryToDecimal function
+		for (int i = 0; i < n; i++) {
+			String s = sc.nextLine();
+			int res = binaryToDecimal(s); //Write binaryToDecimal function
 			System.out.println(res);
 		}
 	}
-	public static int binaryToDecimal(String s){
+	/**.
+	 * { function_description }
+	 *
+	 * @param      s     string
+	 *
+	 * @return     integer
+	 */
+	public static int binaryToDecimal(final String s) {
 		int[] num = new int[s.length()];
-		for(int i=0; i < s.length();i++){
-			num[i] = Integer.parseInt(s.charAt(i)+"");
+		for (int i = 0; i < s.length(); i++) {
+			num[i] = Integer.parseInt(s.charAt(i) + "");
 		}
 		int sum = 0;
-		for(int i = s.length()-1;i>=0;i--){
-			sum = sum + (int)(num[i]*Math.pow(2,((s.length()) - (i +1))));
-		} 
+		for (int i = s.length() - 1; i >= 0; i--) {
+			sum = sum + (int)(num[i] * Math.pow(2, ((s.length()) - (i + 1))));
+		}
 		return sum;
 	}
 
