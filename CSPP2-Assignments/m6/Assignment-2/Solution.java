a86d2c20498e7160302ecb8ae8a805c10e24af51
someshwarthuma <somesh.sandeep619@msitprogram.net>
2018-09-01 10:51:44 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- CSPP2-Assignments/m6/Assignment-2/Solution.java
+++ CSPP2-Assignments/m6/Assignment-2/Solution.java
@@ -23,7 +23,9 @@ final class Solution {
      * @return     Matrix of the rounded elements
      */
 
-    static int[][] roundHundred(final int[][] a, final int rows, final int columns) {
+    static int[][] roundHundred(final int[][] a, 
+                                final int rows, 
+                                final int columns) {
 
         // write ypur code here
         final int fifty = 50;
@@ -32,10 +34,12 @@ final class Solution {
         for (int row = 0; row < rows; row++) {
             for (int col = 0; col < columns; col++) {
                 for (int mul100 = 0; mul100 <= sixHun; mul100 = mul100 + hun) {
-                    if ((mul100 <= a[row][col]) && (a[row][col] < mul100 + fifty)) {
+                    if ((mul100 <= a[row][col]) &&
+                     (a[row][col] < mul100 + fifty)) {
                         a[row][col] = mul100;
                     }
-                    if ((mul100 + fifty <= a[row][col]) && (a[row][col] < mul100 + hun)) {
+                    if ((mul100 + fifty <= a[row][col]) && 
+                        (a[row][col] < mul100 + hun)) {
                         a[row][col] = mul100 + hun;
                     }
                 }
