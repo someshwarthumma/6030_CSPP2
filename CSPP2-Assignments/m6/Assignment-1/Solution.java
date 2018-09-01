1ea9c5574b92f828800f525f310cf70354fccd78
someshwarthuma <somesh.sandeep619@msitprogram.net>
2018-09-01 09:48:55 +0530

    "check_style_score -> 6 of 6 passed. style score: 0/1 "

--- CSPP2-Assignments/m6/Assignment-1/Solution.java
+++ CSPP2-Assignments/m6/Assignment-1/Solution.java
@@ -1,45 +1,51 @@
 import java.util.Scanner;
-/**
+/**.
  * Write a java program to find the odd composite numbers
  * between 2 and n where n is given as an input.
  *
- * @author : 
+ * @author :
  */
 public final class Solution {
-    /**
+    /**.
     * Empty constructor.
     */
     private Solution() {
         //not used
     }
-    /**
+    /**.
      * Prints the odd composite numbers between 2 and n.
      *
-     * @param      n     n value
+     * @param      number     n value
      */
-    static void oddComposites(final int n) {
-	// write your code here
-    	for(int i =3; i <= n; i = i+2){
-    		if(isNotPrime(i)){
-    			System.out.println(i);
-    		}
-    	}
+    static void oddComposites(final int number) {
+        // write your code here
+        for (int index = 3; index <= number; index = index + 2) {
+            if (isNotPrime(index)) {
+                System.out.println(index);
+            }
+        }
     }
-
-    static boolean isNotPrime(final int num){
-    	int flag =0;
-    	for(int i = 1;i<=num; i++){
-    		if (num%i==0){
-    			flag = flag + 1;
-    		}
-    	}
-    	if(flag == 2){
-    		return false;
-    	}else{
-    		return true;
-    	}
+    /**.
+     * Determines if not prime.
+     *
+     * @param      num   input number,which is integer
+     *
+     * @return     True if not prime, False otherwise.
+     */
+    static boolean isNotPrime(final int num) {
+        int count = 0;
+        for (int index = 1; index <= num; index++) {
+            if (num % index == 0) {
+                count = count + 1;
+            }
+        }
+        if (count == 2) {
+            return false;
+        } else {
+            return true;
+        }
     }
-    /**
+    /**.
     * main method as driver program.
     * @param args is the parameter for this method
     */
