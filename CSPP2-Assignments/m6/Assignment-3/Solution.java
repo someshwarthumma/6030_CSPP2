25daf6f7cfa0ea36eaed236417794b940936c47a
someshwarthuma <somesh.sandeep619@msitprogram.net>
2018-09-01 11:10:07 +0530

    "check_style_score -> 5 of 5 passed. style score: 1/1 "

--- CSPP2-Assignments/m6/Assignment-3/Solution.java
+++ CSPP2-Assignments/m6/Assignment-3/Solution.java
@@ -26,7 +26,7 @@ final class Solution {
     static char[] replaceAll(final char[] charArray,
                              final char oldChar, final char newChar) {
         for (int i = 0; i < charArray.length; i++) {
-            if ( charArray[i] == oldChar) {
+            if (charArray[i] == oldChar) {
                 charArray[i] = newChar;
             }
         }
