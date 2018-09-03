cab90509cf702803c3c0d9186587b619abb98f49
someshwarthuma <somesh.sandeep619@msitprogram.net>
2018-09-03 18:36:26 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- CSPP2-Assignments/m7/Assignment-1/Solution.java
+++ CSPP2-Assignments/m7/Assignment-1/Solution.java
@@ -1,27 +1,35 @@
-import java.util.*;
-class InputValidator
-{	String value;
-	InputValidator(String input){
-		this.value = input;
-	}
-
-	boolean validateData(){
-	String x = this.value;
-	int sum = x.length();
-	if (sum >= 6){
-		return true;
-	}
-	return false;
-	}
+import java.util.Scanner;
+/**.
+ * This is input validator class, it validates the input
+ */
+class InputValidator {
+    String value;
+    InputValidator(String input) {
+        this.value = input;
+    }
+    /**.
+     * Checks for  valid data
+     *
+     * @return     { Boolean value }
+     */
+    boolean validateData() {
+        String x = this.value;
+        int sum = x.length();
+        if (sum >= 6) {
+            return true;
+        }
+        return false;
+    }
 }
-public class Solution
-{
-	public static void main(String args[])
-    {
-    	Scanner s=new Scanner(System.in);
-    	String input=s.next();
-    	InputValidator i=new InputValidator(input);    	
-    	System.out.println(i.validateData());
+/**.
+ * This is solution class
+ */
+public class Solution {
+    public static void main(String args[]) {
+        Scanner s = new Scanner(System.in);
+        String input = s.next();
+        InputValidator i = new InputValidator(input);
+        System.out.println(i.validateData());
 
     }
 
