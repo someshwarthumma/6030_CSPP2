dc0ea1776f4fd6b130c9a11575fabd1734765524
someshwarthuma <somesh.sandeep619@msitprogram.net>
2018-09-06 10:15:46 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- CSPP2-Assignments/m10/Assignment-2/Solution.java
+++ CSPP2-Assignments/m10/Assignment-2/Solution.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 /**.
  * This is solution for the class  to implement ListADT of type String
  */
-public class Solution {
+public final class Solution {
     /**.
      * This is private construtor
      */
-    private Solution(){
+    private Solution() {
         //This is solution
     }
 
@@ -17,10 +17,10 @@ public class Solution {
      *
      * @param      args  Argument
      */
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         // create an object of the list to invoke methods on it
         StringList sl = new StringList();
-        StringListInterface l = (StringListInterface)sl; //Typecasting is done for StringListInterface
+        StringListInterface l = (StringListInterface) sl;
         // code to read the test cases input file
         Scanner stdin = new Scanner(new BufferedInputStream(System.in));
         // check if there is one more line to process
