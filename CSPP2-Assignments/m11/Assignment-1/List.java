4b6889ee3e9b54b71711a372bdb060aa1e138ec5
someshwarthuma <somesh.sandeep619@msitprogram.net>
2018-09-07 15:59:26 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- CSPP2-Assignments/m11/Assignment-1/List.java
+++ CSPP2-Assignments/m11/Assignment-1/List.java
@@ -445,7 +445,7 @@ public class List {
                     l.removeAll(a);
                 }
                 break;
-            case "subList": 
+            case "subList":
                 if (tokens.length != 2) {
                     break;
                 }
@@ -456,7 +456,6 @@ public class List {
                     System.out.println(object);
                 }
                 break;
-            
             case "equals":
                 if (tokens.length == 2) {
                     String[] lt = tokens[1].split(",");
