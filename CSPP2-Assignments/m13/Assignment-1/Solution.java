a28dfd6c89e29cebfaa147596563e7f5734aaf58
someshwarthuma <somesh.sandeep619@msitprogram.net>
2018-09-08 12:28:14 +0530

    "check_style_score -> 3 of 4 passed. style score: 0/1 "

--- CSPP2-Assignments/m13/Assignment-1/Solution.java
+++ CSPP2-Assignments/m13/Assignment-1/Solution.java
@@ -2,26 +2,45 @@ import java.io.BufferedInputStream;
 import java.util.Scanner;
 import java.util.Arrays;
 
-/**
+/**.
  * Class for set.
- * @author :
+ * @author :T someshwar
  */
 class Set {
     //your code goes here...
     //Good luck :-)
+    
+    /**.
+     * { This is list of inttype }
+     */
     public int[] list;
+    /**.
+     * This is size
+     */
     public int size;
-
+    /**.
+     * This is constructor
+     */
     public Set() {
         list = new int[10];
         size = 0;
     }
-
+    /**.
+     * This is size method
+     *
+     * @return     { description_of_the_return_value }
+     */
     public int size() {
         return size;
     }
-
-    public boolean contains(int item) {
+    /**.
+     * This is contains method
+     *
+     * @param      item  The item
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public boolean contains(final int item) {
         for (int i = 0; i < size; i++) {
             if (list[i] == item) {
                 return true;
@@ -29,7 +48,11 @@ class Set {
         }
         return false;
     }
-
+    /**.
+     * Thiss is tostring
+     *
+     * @return     String representation of the object.
+     */
     public String toString() {
         if (size == 0) {
             return "{}";
@@ -40,8 +63,12 @@ class Set {
         }
         return str + list[size - 1] + "}";
     }
-
-    public void add(int item) {
+    /**.
+     * Thisis add method
+     *
+     * @param      item  The item
+     */
+    public void add(final int item) {
         if (size == list.length) {
             resize();
         }
@@ -49,22 +76,40 @@ class Set {
             list[size++] = item;
         }
     }
-
+    /**.
+     * This is resize
+     */
     private void resize() {
         list = Arrays.copyOf(list, 2 * list.length);
     }
-
-    public void addAll(int[] array) {
+    /**.
+     * This is addAll
+     *
+     * @param      array  The array
+     */
+    public void addAll(final int[] array) {
         for (int i = 0; i < array.length; i++) {
             add(array[i]);
         }
     }
-
-    public int get(int index) {
+    /**.
+     * This is get methd
+     *
+     * @param      index  The index
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int get(final int index) {
         return list[index];
     }
-
-    public Set intersection(Set givenSet) {
+    /**.
+     * This is intersection method
+     *
+     * @param      givenSet  The given set
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public Set intersection(final Set givenSet) {
         Set intersect = new Set();
         for (int i = 0; i < this.size; i++) {
             if (givenSet.contains(this.get(i))) {
@@ -74,8 +119,14 @@ class Set {
         return intersect;
 
     }
-
-    public Set retainAll(int[] array) {
+    /**.
+     * This is retain ALl method
+     *
+     * @param      array  The array
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public Set retainAll(final int[] array) {
         Set retain = new Set();
         Set newArray = new Set();
         newArray.addAll(array);
@@ -86,8 +137,14 @@ class Set {
         }
         return retain;
     }
-
-    public int[][] cartesianProduct(Set array) {
+    /**.
+     * This is cartesian product funtion
+     *
+     * @param      array  The array
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int[][] cartesianProduct(final Set array) {
         int length = this.size * array.size;
         int[][] cartesian = new int[length][2];
         int i= 0;
@@ -110,17 +167,17 @@ class Set {
     }
 
 }
-/**
+/**.
  * Solution class for code-eval.
  */
 public final class Solution {
-    /**
+    /**.
      * Constructs the object.
      */
     private Solution() {
 
     }
-    /**
+    /**.
      * helper function to convert string input to int array.
      *
      * @param      s     { string input from test case file }
@@ -139,7 +196,7 @@ public final class Solution {
                .mapToInt(Integer::parseInt)
                .toArray();
     }
-    /**
+    /**.
      * main function to execute test cases.
      *
      * @param      args  The arguments
