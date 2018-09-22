54d4818dc303120e94a8add503ca95c7536070bf
someshwarthuma <somesh.sandeep619@msitprogram.net>
2018-09-22 12:53:06 +0530

    "check_style_score -> 14 of 14 passed. style score: 1/1 "

--- CSPP2-Assignments/m25/Assignment-1/Task.java
+++ CSPP2-Assignments/m25/Assignment-1/Task.java
@@ -1,82 +1,148 @@
-public class Task{
-	String title;
-	String assignedTo;
-	int timeToComplete;
-	boolean important ;
-	boolean urgent;
-	String status;
-	Task(String t,String a, int time,boolean imp, boolean urg, String statu) throws Exception{
-		if(t.length()!=0){
-			this.title = t;
-		} else {
-			throw new Exception("Title not provided");
-		}
-		this.assignedTo = a;
-		if(time>=0){
-			this. timeToComplete = time;
-		} else {
-			throw new Exception("Invalid timeToComplete "+time);
-		}
-		this.important = imp;
-		this.urgent = urg;
-		if(statu.equals("todo") || statu.equals("done")){
-			this.status = statu;
-		} else{
-			throw new Exception("Invalid status "+statu);
-		}
-	}
+/**.
+ * This is task
+ */
+public class Task {
+    /**.
+     * This is title
+     */
+    private String title;
+    /**.
+     * This is assignedTo
+     */
+    private String assignedTo;
+    /**.
+     * This is time
+     */
+    private int timeToComplete;
+    /**.
+     * This is important flag
+     */
+    private boolean important ;
+    /**.
+     * This is urgent flag
+     */
+    private boolean urgent;
+    /**.
+     * This si status
+     */
+    private String status;
+    /**.
+     * This si task constructor
+     *
+     * @param      t      { task name }
+     * @param      a      { name }
+     * @param      time   time
+     * @param      imp    imp flag
+     * @param      urg    urg flag
+     * @param      statu  status flag
+     */
+    Task(String t, String a, int time, boolean imp, boolean urg, String statu) throws Exception {
+        if (t.length() != 0) {
+            this.title = t;
+        } else {
+            throw new Exception("Title not provided");
+        }
+        this.assignedTo = a;
+        if (time >= 0) {
+            this. timeToComplete = time;
+        } else {
+            throw new Exception("Invalid timeToComplete " + time);
+        }
+        this.important = imp;
+        this.urgent = urg;
+        if (statu.equals("todo") || statu.equals("done")) {
+            this.status = statu;
+        } else {
+            throw new Exception("Invalid status " + statu);
+        }
+    }
+    /**.
+     * This is getname 
+     *
+     * @return     The name.
+     */
+    public String getName() {
+        return this.assignedTo;
+    }
+    /**.
+     * This is getimportant
+     *
+     * @return     The important.
+     */
+    public boolean getImportant() {
+        return this.important;
+    }
+    /**.
+     * This is getUrgent
+     *
+     * @return     The urgent.
+     */
+    public boolean getUrgent() {
+        return this.urgent;
+    }
+    /**.
+     * This is get status
+     *
+     * @return     The status.
+     */
+    public String getStatus() {
+        return this.status;
+    }
+    /**.
+     * This is getTime
+     *
+     * @return     The time.
+     */
+    public int getTime() {
+        return this.timeToComplete;
+    }
+    /**.
+     * This is toString function
+     */
+    public void tostring() {
+        String urgentflag;
+        String importantflag;
 
-	public String getName(){
-		return this.assignedTo;
-	}
-	public boolean getImportant(){
-		return this.important;
-	}
-	public boolean getUrgent(){
-		return this.urgent;
-	}
-	public String getStatus(){
-		return this.status;
-	}
-	public int getTime(){
-		return this.timeToComplete;
-	}
+        if (urgent == true) {
+            urgentflag = "Urgent";
+        } else {
+            urgentflag = "Not Urgent";
+        }
+        if (important == true) {
+            importantflag = "Important";
+        } else {
+            importantflag = "Not Important";
+        }
 
-	public void tostring(){
-		String urgentflag;
-		String importantflag;
-		
-		if(urgent == true){
-			urgentflag = "Urgent";
-		} else {
-			urgentflag = "Not Urgent";
-		}
-		if(important == true){
-			importantflag = "Important";
-		} else {
-			importantflag = "Not Important";
-		}
+        System.out.println(title +
+            ", " + assignedTo + ", "
+            + timeToComplete + ", "
+            + importantflag + ", "
+            + urgentflag + ", "
+            + status);
+        
+    }
+    /**.
+     * this is getPrint funtiocn
+     *
+     * @return     The print.
+     */
+    public String getPrint() {
+        String urgentflag;
+        String importantflag;
 
-		System.out.println(title+", "+assignedTo+", "+timeToComplete+", "+importantflag+", "+urgentflag+", "+status);
-		//return title+", "+assignedTo+", "+timeToComplete+", "+importantflag+", "+urgentflag+", "+status;
-	}
-
-	public String getPrint(){
-		String urgentflag;
-		String importantflag;
-		
-		if(urgent == true){
-			urgentflag = "Urgent";
-		} else {
-			urgentflag = "Not Urgent";
-		}
-		if(important == true){
-			importantflag = "Important";
-		} else {
-			importantflag = "Not Important";
-		}
-
-		//System.out.println(title+", "+assignedTo+", "+timeToComplete+", "+importantflag+", "+urgentflag+", "+status);
-		return title+", "+assignedTo+", "+timeToComplete+", "+importantflag+", "+urgentflag+", "+status;
-	}
+        if (urgent == true) {
+            urgentflag = "Urgent";
+        } else {
+            urgentflag = "Not Urgent";
+        }
+        if (important == true) {
+            importantflag = "Important";
+        } else {
+            importantflag = "Not Important";
+        }
+        return title + ", " + assignedTo
+        + ", " + timeToComplete + ", " + importantflag
+        + ", " + urgentflag + ", " + status;
+    }
 }
--- CSPP2-Assignments/m25/Assignment-1/Todoist.java
+++ CSPP2-Assignments/m25/Assignment-1/Todoist.java
@@ -1,13 +1,21 @@
+/**.
+ * This is todoist class
+ */
 public class Todoist{
 	static List<Task>taskList = new List<Task>();
 	Todoist(){
 
 	}
-
+	/**.
+	 *
+	 * @param      task  The task
+	 */
 	public void addTask(Task task){
 		taskList.add(task);
 	}
-
+	/**.
+	 * This is tostring method
+	 */
 	public void tostring(){
 		for(int i=0;i<taskList.size();i++){
 			System.out.println(taskList.get(i).getPrint()); 
@@ -15,7 +23,13 @@ public class Todoist{
 	}
 
 
-
+	/**.
+	 * This is getNextTask.
+	 *
+	 * @param      name  The name
+	 *
+	 * @return     The next task.
+	 */
 	public String getNextTask(String name){
 		for(int i =0;i<taskList.size();i++){
 			if(name.equals(taskList.get(i).getName())){
@@ -29,7 +43,11 @@ public class Todoist{
 		//System.out.println("This is end: ");
 		return "null";
 	}
-
+	/**.
+	 * This is totalTime method
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
 	public int totalTime4Completion(){
 		int sum =0;
 		for(int i =0 ;i<taskList.size();i++){
@@ -39,7 +57,14 @@ public class Todoist{
 		}
 		return sum;
 	}
-
+	/**.
+	 * This is getNext task
+	 *
+	 * @param      name   The name
+	 * @param      count  The count
+	 *
+	 * @return     The next task.
+	 */
 	public String[] getNextTask(String name, int count){
 		String[] arr = new String[count];
 		int j = 0;
