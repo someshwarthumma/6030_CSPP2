718ca6fcd6690331872dc0587101f0cc1d5d314c
someshwarthuma <somesh.sandeep619@msitprogram.net>
2018-09-22 11:39:36 +0530

    "check_style_score -> 2 of 14 passed. style score: 0/1 "

--- CSPP2-Assignments/m25/Assignment-1/Task.java
+++ CSPP2-Assignments/m25/Assignment-1/Task.java
@@ -50,7 +50,7 @@ public class Task{
 		return title+", "+assignedTo+", "+timeToComplete+", "+importantflag+", "+urgentflag+", "+status;
 	}
 
-	public String getPrint(){
+	public void getPrint(){
 		String urgentflag;
 		String importantflag;
 		
@@ -65,7 +65,7 @@ public class Task{
 			importantflag = "Not Important";
 		}
 
-		// System.out.println(title+", "+assignedTo+", "+timeToComplete+", "+importantflag+", "+urgentflag+", "+statusflag);
-		return title+", "+assignedTo+", "+timeToComplete+", "+importantflag+", "+urgentflag+", "+status;
+		 System.out.println(title+", "+assignedTo+", "+timeToComplete+", "+importantflag+", "+urgentflag+", "+status);
+		//return title+", "+assignedTo+", "+timeToComplete+", "+importantflag+", "+urgentflag+", "+status;
 	}
 }
